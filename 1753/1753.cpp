6dbf3155e4833e1597e264c1e141a9c27444cc23
Amir Farukshin <farukshin.amir@gmail.com>
2020-09-03 22:31:15 +0300

    docs(): реализовал алгоритмом Ахо-Корасик, снова 3 TL, нужен препроцессинг текста

--- 1753/1753.cpp
+++ 1753/1753.cpp
@@ -6,26 +6,145 @@ using namespace std;
 typedef long long ll;
 typedef long double ld;
 
+const int ALPHABET_SIZE = 26;
+
+struct vertex
+{
+    vertex* next[ALPHABET_SIZE];
+    vertex* go[ALPHABET_SIZE];
+    vertex* link = 0;
+    vertex* parent;
+    vertex* prev;
+    int parent_char;
+    vector<int> ind;
+
+    vertex();
+    vertex(int parent_char, vertex* parent);
+};
+
+vertex::vertex()
+{
+    parent_char = 0;
+    parent = nullptr;
+    prev = nullptr;
+    for (int i = 0; i < ALPHABET_SIZE; ++i)
+    {
+        next[i] = nullptr;
+        go[i] = nullptr;
+    }
+}
+
+vertex::vertex(int parent_char, vertex* parent)
+{
+    this->parent_char = parent_char;
+    this->parent = parent;
+    prev = nullptr;
+    for (int i = 0; i < ALPHABET_SIZE; ++i)
+    {
+        next[i] = nullptr;
+        go[i] = nullptr;
+    }
+}
+
+vertex* root = new vertex(-1, 0);
+
+void add_string(string& line, int ind)
+{
+    vertex* current = root;
+    int n = line.length();
+    for (int i = 0; i < n; ++i)
+    {
+        int symbol = line[i] - 'a';
+        if (current->next[symbol] == nullptr)
+        {
+            current->next[symbol] = new vertex(symbol, current);
+        }
+        current = current->next[symbol];
+    }
+    current->ind.push_back(ind);
+}
+
+vertex* go(vertex* current, int c);
+
+vertex* link(vertex* current)
+{
+    if (!current->link)
+    {
+        if (current == root || current->parent == root)
+        {
+            current->link = root;
+        }
+        else
+        {
+            current->link = go(link(current->parent), current->parent_char);
+        }
+    }
+    return current->link;
+}
+
+vertex* go(vertex* current, int c)
+{
+    if (!current->go[c])
+    {
+        if (current->next[c])
+        {
+            current->go[c] = current->next[c];
+        }
+        else if (current == root)
+        {
+            current->go[c] = root;
+        }
+        else
+        {
+            current->go[c] = go(link(current), c);
+        }
+    }
+    return current->go[c];
+}
+
+vertex* prev(vertex* current)
+{
+    if (current == root)
+    {
+        return nullptr;
+    }
+    if (!current->prev)
+    {
+        if (link(current)->ind.size() != 0)
+        {
+            current->prev = link(current);
+        }
+        else
+        {
+            current->prev = prev(link(current));
+        }
+    }
+    return current->prev;
+}
+
 void solve()
 {
-    string s, pattern;
-    cin >> s >> pattern;
+    string text, pattern;
+    cin >> text >> pattern;
 
-    int ans = 0, cur = 0, lenPattern = pattern.length();
-    if (lenPattern > s.length())
+    add_string(pattern, 0);
+
+    vector<vector<int>> ans(1);
+    vertex* cur = root;
+    for (int i = 0; i < text.size(); i++)
     {
-        cout << ans << endl;
-        return;
+        cur = go(cur, text[i] - 'a');
+        vertex* cur_ans = cur;
+        while (cur_ans)
+        {
+            for (int ind : cur_ans->ind)
+                ans[ind].push_back(i + 1);
+            cur_ans = prev(cur_ans);
+        }
     }
 
-    for (int i = 0; i < s.length() - lenPattern + 1; i++)
-        for (int j = 0; j < lenPattern; j++)
-            if (s[i + j] == pattern[j] && j == lenPattern - 1)
-                ans++;
-            else if (s[i + j] != pattern[j])
-                break;
+    cout << ans[0].size() << endl;
 
-    cout << ans << endl;
 }
 
 int main()
