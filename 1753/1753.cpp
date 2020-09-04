3ad0aac1bc2a2c256ae2521d0bafa8012bd64817
Amir Farukshin <farukshin.amir@gmail.com>
2020-09-04 20:32:33 +0300

    docs(): решил с помощью Z функции, полное решение

--- 1753/1753.cpp
+++ 1753/1753.cpp
@@ -1,125 +1,23 @@
 //https://cses.fi/problemset/task/1753
-//#tech_debt
 #include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
 typedef long double ld;
 
-const int ALPHABET_SIZE = 26;
-
-struct vertex
+void zFunction(const string s, vector<int>& z)
 {
-    vertex* next[ALPHABET_SIZE];
-    vertex* go[ALPHABET_SIZE];
-    vertex* link = 0;
-    vertex* parent;
-    vertex* prev;
-    int parent_char;
-    vector<int> ind;
-
-    vertex();
-    vertex(int parent_char, vertex* parent);
-};
-
-vertex::vertex()
-{
-    parent_char = 0;
-    parent = nullptr;
-    prev = nullptr;
-    for (int i = 0; i < ALPHABET_SIZE; ++i)
+    int n = (int)s.length();
+    z.resize(n);
+    for (int i = 1, l = 0, r = 0; i < n; ++i)
     {
-        next[i] = nullptr;
-        go[i] = nullptr;
+        if (i <= r)
+            z[i] = min(r - i + 1, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            ++z[i];
+        if (i + z[i] - 1 > r)
+            l = i, r = i + z[i] - 1;
     }
-}
-
-vertex::vertex(int parent_char, vertex* parent)
-{
-    this->parent_char = parent_char;
-    this->parent = parent;
-    prev = nullptr;
-    for (int i = 0; i < ALPHABET_SIZE; ++i)
-    {
-        next[i] = nullptr;
-        go[i] = nullptr;
-    }
-}
-
-vertex* root = new vertex(-1, 0);
-
-void add_string(string& line, int ind)
-{
-    vertex* current = root;
-    int n = line.length();
-    for (int i = 0; i < n; ++i)
-    {
-        int symbol = line[i] - 'a';
-        if (current->next[symbol] == nullptr)
-        {
-            current->next[symbol] = new vertex(symbol, current);
-        }
-        current = current->next[symbol];
-    }
-    current->ind.push_back(ind);
-}
-
-vertex* go(vertex* current, int c);
-
-vertex* link(vertex* current)
-{
-    if (!current->link)
-    {
-        if (current == root || current->parent == root)
-        {
-            current->link = root;
-        }
-        else
-        {
-            current->link = go(link(current->parent), current->parent_char);
-        }
-    }
-    return current->link;
-}
-
-vertex* go(vertex* current, int c)
-{
-    if (!current->go[c])
-    {
-        if (current->next[c])
-        {
-            current->go[c] = current->next[c];
-        }
-        else if (current == root)
-        {
-            current->go[c] = root;
-        }
-        else
-        {
-            current->go[c] = go(link(current), c);
-        }
-    }
-    return current->go[c];
-}
-
-vertex* prev(vertex* current)
-{
-    if (current == root)
-    {
-        return nullptr;
-    }
-    if (!current->prev)
-    {
-        if (link(current)->ind.size() != 0)
-        {
-            current->prev = link(current);
-        }
-        else
-        {
-            current->prev = prev(link(current));
-        }
-    }
-    return current->prev;
 }
 
 void solve()
@@ -127,24 +25,15 @@ void solve()
     string text, pattern;
     cin >> text >> pattern;
 
-    add_string(pattern, 0);
+    vector<int> z;
+    zFunction(pattern + '$' + text, z);
 
-    vector<vector<int>> ans(1);
-    vertex* cur = root;
-    for (int i = 0; i < text.size(); i++)
-    {
-        cur = go(cur, text[i] - 'a');
-        vertex* cur_ans = cur;
-        while (cur_ans)
-        {
-            for (int ind : cur_ans->ind)
-                ans[ind].push_back(i + 1);
-            cur_ans = prev(cur_ans);
-        }
-    }
+    int ans = 0;
+    for (int i = 0; i <= (int)text.length() - 1; i++)
+        if (z[i + (int)pattern.length() + 1] == (int)pattern.length())
+            ans++;
 
-    cout << ans[0].size() << endl;
-
+    cout << ans << endl;
 }
 
 int main()
